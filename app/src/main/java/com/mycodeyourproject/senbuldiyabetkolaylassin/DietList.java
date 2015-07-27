d38ef33a7e965b5aad09e89989f974b5f43ee971
onculy <onculy@gmail.com>
2015-07-27 12:24:03 +0300

    Diet List

--- app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/DietList.java
+++ app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/DietList.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class DietList extends AppCompatActivity {
 
@@ -33,5 +34,10 @@ public class DietList extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void button(View view)
+    {
+
     }
 }
