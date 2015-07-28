ea7c011ce7aa71c0874c018783f7364743e54fcf
onculy <onculy@gmail.com>
2015-07-28 17:27:54 +0300

    Charts

---
+++ app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/Stats.java
@@ -0,0 +1,95 @@
+package com.mycodeyourproject.senbuldiyabetkolaylassin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import java.util.ArrayList;
+
+public class Stats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+        chart.setDescription("ViaDiabet Chart");
+
+        ArrayList<Entry> valsComp1 = new ArrayList<Entry>();
+        ArrayList<Entry> valsComp2 = new ArrayList<Entry>();
+        Entry c1e1 = new Entry(100.000f, 0); // 0 == quarter 1
+        valsComp1.add(c1e1);
+        Entry c1e2 = new Entry(50.000f, 1); // 1 == quarter 2 ...
+        valsComp1.add(c1e2);
+        // and so on ...
+
+        Entry c2e1 = new Entry(120.000f, 0); // 0 == quarter 1
+        valsComp2.add(c2e1);
+        Entry c2e2 = new Entry(110.000f, 1); // 1 == quarter 2 ...
+        valsComp2.add(c2e2);
+        LineDataSet setComp1 = new LineDataSet(valsComp1, "Company 1");
+        setComp1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        LineDataSet setComp2 = new LineDataSet(valsComp2, "Company 2");
+        setComp2.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(setComp1);
+        dataSets.add(setComp2);
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("1.Q"); xVals.add("2.Q"); xVals.add("3.Q"); xVals.add("4.Q");
+
+        LineData data = new LineData(xVals, dataSets);
+        chart.setData(data);
+        chart.invalidate(); // refresh
+
+        PieChart pie = (PieChart) findViewById(R.id.chartPie);
+        pie.setDescription("piechart");
+        float values[] = {30, 30, 40};
+        pie.setUsePercentValues(true);
+        ArrayList<Entry> vals = new ArrayList<Entry>();
+        vals.add(new Entry(30, 0));
+        vals.add(new Entry(30, 0));
+        vals.add(new Entry(40, 0));
+
+        PieDataSet pieDataSet = new PieDataSet(vals, "market set");
+        PieData pieData = new PieData(new String[]{"a", "b", "c"}, pieDataSet);
+        pie.setData(pieData);
+        pie.invalidate();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_stats, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/ViaDiabet.java
+++ app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/ViaDiabet.java
@@ -29,27 +29,25 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 
-public class ViaDiabet extends BaseViaDiabetActivity
-{
+public class ViaDiabet extends BaseViaDiabetActivity {
     public final static String MESAJ = "com.mycodeyourproject.senbuldiyabetkolaylassin.MESAJ";
 
-    private void setInitialValues()
-    {
+    private void setInitialValues() {
         SimpleDateFormat sdfDate = new SimpleDateFormat((getString(R.string.date_format)));
         SimpleDateFormat sdfTime = new SimpleDateFormat((getString(R.string.time_format)));
 
-        TextView lastCheckBlood=(TextView)findViewById(R.id.text_last_check_blood);
+        TextView lastCheckBlood = (TextView) findViewById(R.id.text_last_check_blood);
         lastCheckBlood.setText(getString(R.string.number_zero));
 
-        TextView lastCheckDate=(TextView)findViewById(R.id.text_last_check_date);
+        TextView lastCheckDate = (TextView) findViewById(R.id.text_last_check_date);
         lastCheckDate.setText(sdfDate.format(Calendar.getInstance().getTime()));
 
-        TextView lastCheckInsulinDate=(TextView)findViewById(R.id.text_last_check_insulin_date);
+        TextView lastCheckInsulinDate = (TextView) findViewById(R.id.text_last_check_insulin_date);
         lastCheckInsulinDate.setText(sdfDate.format(Calendar.getInstance().getTime()));
-        TextView autoCommentTime=(TextView)findViewById(R.id.text_auto_comment_time);
+        TextView autoCommentTime = (TextView) findViewById(R.id.text_auto_comment_time);
         autoCommentTime.setText(sdfTime.format(Calendar.getInstance().getTime()));
 
-        TextView lastCheckInsuline=(TextView)findViewById(R.id.text_last_check_insulin);
+        TextView lastCheckInsuline = (TextView) findViewById(R.id.text_last_check_insulin);
 
         String firstString = getString(R.string.number_zero);
         String secondString = getString(R.string.unity_insuline);
@@ -61,7 +59,7 @@ public class ViaDiabet extends BaseViaDiabetActivity
                 firstString.length() + secondString.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
         lastCheckInsuline.setText(stringBuilder);
 
-        TextView autoComment=(TextView)findViewById(R.id.text_auto_comment);
+        TextView autoComment = (TextView) findViewById(R.id.text_auto_comment);
 
         String firstComment = getString(R.string.auto_comment_hey);
         String secondComment = getString(R.string.auto_comment_need_more_values);
@@ -81,108 +79,139 @@ public class ViaDiabet extends BaseViaDiabetActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_via_diabet, menu);
         return true;
     }
 
     /**
      * On selecting action bar icons
-     * */
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Take appropriate action for each action item click
-        switch (item.getItemId())
-        {
+        switch (item.getItemId()) {
             case R.id.action_my_info:
-                Toast.makeText(this,R.string.action_my_info,Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.action_my_info, Toast.LENGTH_LONG).show();
                 return true;
 
             case R.id.action_logs:
-                Toast.makeText(this,R.string.action_logs,Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.action_logs, Toast.LENGTH_LONG).show();
                 return true;
             case R.id.action_tools:
-                Toast.makeText(this,R.string.action_tools,Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.action_tools, Toast.LENGTH_LONG).show();
                 return true;
 
             case R.id.action_settings:
-                Intent intent=new Intent(ViaDiabet.this,Settings.class);
+                Intent intent = new Intent(ViaDiabet.this, Settings.class);
                 /*PREFERENCE ACTIVITY de bu sekilde yapilmali
                 Intent intent=new Intent(ViaDiabet.this,Preferences.class);*/
                 startActivity(intent);
                 return true;
 
             case R.id.action_about:
-                Toast.makeText(this,R.string.action_about,Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.action_about, Toast.LENGTH_LONG).show();
                 return true;
             case R.id.action_help:
-                Toast.makeText(this,R.string.action_help,Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.action_help, Toast.LENGTH_LONG).show();
                 return true;
             case R.id.action_twitter:
-                try
-                {
-                    ApplicationInfo info = getPackageManager().getApplicationInfo("com.twitter.android", 0 );
-                    PackageManager pm=getPackageManager();
-                    Intent twitter=pm.getLaunchIntentForPackage("com.twitter.android");
+                try {
+                    ApplicationInfo info = getPackageManager().getApplicationInfo("com.twitter.android", 0);
+                    PackageManager pm = getPackageManager();
+                    Intent twitter = pm.getLaunchIntentForPackage("com.twitter.android");
                     startActivity(twitter);
-                }
-                catch( PackageManager.NameNotFoundException e )
-                {
-                    Toast.makeText(this,R.string.action_app_not_found,Toast.LENGTH_LONG).show();
+                } catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(this, R.string.action_app_not_found, Toast.LENGTH_LONG).show();
                     return false;
                 }
                 return true;
 
             case R.id.action_facebook:
-                try
-                {
-                    ApplicationInfo info = getPackageManager().getApplicationInfo("com.facebook.android", 0 );
-                    PackageManager pm=getPackageManager();
-                    Intent facebook=pm.getLaunchIntentForPackage("com.facebook.android");
+                try {
+                    ApplicationInfo info = getPackageManager().getApplicationInfo("com.facebook.android", 0);
+                    PackageManager pm = getPackageManager();
+                    Intent facebook = pm.getLaunchIntentForPackage("com.facebook.android");
                     startActivity(facebook);
-                }
-                catch( PackageManager.NameNotFoundException e )
-                {
+                } catch (PackageManager.NameNotFoundException e) {
                     return false;
                 }
                 return true;
             case R.id.action_updates:
-                Toast.makeText(this,R.string.action_check_updates,Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.action_check_updates, Toast.LENGTH_LONG).show();
                 return true;
             case R.id.action_logout:
-                Toast.makeText(this,R.string.action_logout,Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.action_logout, Toast.LENGTH_LONG).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void createNotification(View v) {
+    public void showNotif(View v)
+    {
+        createNotification();
+    }
 
+    private void createNotification() {
+        // BEGIN_INCLUDE(notificationCompat)
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        // END_INCLUDE(notificationCompat)
 
+        // BEGIN_INCLUDE(intent)
+        //Create Intent to launch this Activity again if the notification is clicked.
         Intent i = new Intent(this, ViaDiabet.class);
         i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent intent = PendingIntent.getActivity(this, 0, i,
                 PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(intent);
+        // END_INCLUDE(intent)
 
-        builder.setTicker("Şekerini ölç");
+        // BEGIN_INCLUDE(ticker)
+        // Sets the ticker text
+        builder.setTicker(getResources().getString(R.string.custom_notification));
 
+        // Sets the small icon for the ticker
         builder.setSmallIcon(R.drawable.cast_ic_notification_0);
+        // END_INCLUDE(ticker)
 
+        // BEGIN_INCLUDE(buildNotification)
+        // Cancel the notification when clicked
         builder.setAutoCancel(true);
 
+        // Build the notification
         Notification notification = builder.build();
+        // END_INCLUDE(buildNotification)
 
+        // BEGIN_INCLUDE(customLayout)
+        // Inflate the notification layout as RemoteViews
+
+        // Set text on a TextView in the RemoteViews programmatically.
+
+        /* Workaround: Need to set the content view here directly on the notification.
+         * NotificationCompatBuilder contains a bug that prevents this from working on platform
+         * versions HoneyComb.
+         * See https://code.google.com/p/android/issues/detail?id=30495
+         */
+        //notification.contentView = contentView;
+
+        // Add a big content view to the notification if supported.
+        // Support for expanded notifications was added in API level 16.
+        // (The normal contentView is shown when the notification is collapsed, when expanded the
+        // big content view set here is displayed.)
         if (Build.VERSION.SDK_INT >= 16) {
+            // Inflate and set the layout for the expanded notification view
             RemoteViews expandedView =
                     new RemoteViews(getPackageName(), R.layout.notification_expanded);
             notification.bigContentView = expandedView;
         }
+        // END_INCLUDE(customLayout)
+
+        // START_INCLUDE(notify)
+        // Use the NotificationManager to show the notification
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         nm.notify(0, notification);
+        // END_INCLUDE(notify)
     }
 
     //BURASI PREFERENCEACTIVITY KULLANILACAGI ZAMAN BU SEKILDE YAPILMALI
