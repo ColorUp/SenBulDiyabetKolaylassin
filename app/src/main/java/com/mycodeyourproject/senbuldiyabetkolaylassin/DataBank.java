b7496b11af3bce0842f11a6f91ae9db84e529fc7
ColorUp <colorupstudio@gmail.com>
2015-07-30 02:09:52 +0300

    Veri bankası oluşturuldu

---
+++ app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/DataBank.java
@@ -0,0 +1,16 @@
+package com.mycodeyourproject.senbuldiyabetkolaylassin;
+
+import android.os.Bundle;
+
+/**
+ * Created by Burak on 30.07.2015.
+ */
+public class DataBank extends BaseViaDiabetActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_bank);
+    }
+}
