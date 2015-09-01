6175f194da52ec14748bd35909035a0eb7b03417
ColorUp <colorupstudio@gmail.com>
2015-09-02 00:40:50 +0300

    Progress bar içeren action bar yaratıldı

--- app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/BaseViaDiabetActivity.java
+++ app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/BaseViaDiabetActivity.java
@@ -1,18 +1,75 @@
 package com.mycodeyourproject.senbuldiyabetkolaylassin;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by Burak on 12.07.2015.
  */
 public class BaseViaDiabetActivity extends ActionBarActivity
 {
+    ActionBar mActionBar;
+    View mCustomView;
+    Handler handler = new Handler();
+    ProgressBar mProgress;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.show();
+
+        mActionBar = getSupportActionBar();
+        mActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater mInflater = LayoutInflater.from(this);
+
+        mCustomView = mInflater.inflate(R.layout.actionbar_with_progressbar, null);
+    }
+
+    public void SetProgressBarTextValueAndShow(String string, final Integer progressValue)
+    {
+        mProgress = (ProgressBar) mCustomView.findViewById(R.id.progressbar);
+
+        new Thread(new Runnable() {
+
+            int i = 0;
+            int progressStatus = 0;
+
+            public void run() {
+                while (progressStatus < progressValue) {
+                    progressStatus += doWork();
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Update the progress bar
+                    handler.post(new Runnable() {
+                        public void run() {
+                            mProgress.setProgress(progressStatus);
+                            i++;
+                        }
+                    });
+                }
+            }
+            private int doWork() {
+
+                return i;
+            }
+
+        }).start();
+
+        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.title_text);
+        mTitleTextView.setText(string);
+
+        mActionBar.setCustomView(mCustomView);
+        mActionBar.setDisplayShowCustomEnabled(true);
     }
 }
--- app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/SignupActivity.java
+++ app/src/main/java/com/mycodeyourproject/senbuldiyabetkolaylassin/SignupActivity.java
@@ -1,13 +1,17 @@
 package com.mycodeyourproject.senbuldiyabetkolaylassin;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.net.URLEncoder;
@@ -23,41 +27,22 @@ import java.util.Map;
 public class SignupActivity extends BaseViaDiabetActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_signup, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        this.SetProgressBarTextValueAndShow("Kullanıcı Bilgileri (1/2)", 50);
     }
 
     public void signUp(View v)
     {
         Button btnSave=(Button)findViewById(R.id.buttonSave);
-        EditText txtUserName=(EditText)findViewById(R.id.editTextUserName);
-        EditText txtPassword=(EditText)findViewById(R.id.editTextPassword);
-        EditText txtPasswordAgain=(EditText)findViewById(R.id.editTextPasswordAgain);
-        EditText txtEmail=(EditText)findViewById(R.id.editTextEmail);
-        EditText txtPhoneNumber=(EditText)findViewById(R.id.editTextPhoneNumber);
+        EditText txtUserName=(EditText)findViewById(R.id.signup_username).findViewById(R.id.textbox_editText);
+        EditText txtPassword=(EditText)findViewById(R.id.signup_password).findViewById(R.id.textbox_editText);
+        EditText txtPasswordAgain=(EditText)findViewById(R.id.signup_password_again).findViewById(R.id.textbox_editText);
+        EditText txtEmail=(EditText)findViewById(R.id.signup_email).findViewById(R.id.textbox_editText);
+        EditText txtPhoneNumber=(EditText)findViewById(R.id.signup_phone_number).findViewById(R.id.textbox_editText);
 
         Date birthDate=new Date(2000,12,23);
 
